095b323e0b49e7f0068302ca04cd470c45544536
Renan Victor <80129254+Renanvictordiascosta@users.noreply.github.com>
2024-07-24 17:48:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+  // Cálculo básico em PHP
+  
+  $variavel1 = 8;
+  $variavel1++;
+
+  $variavel2 = 2;
+  $variavel2++;
+
+  $variavel3 = $variavel1 - $variavel2;
+
+  echo "Resultado = {$variavel3}";
+
+?>
