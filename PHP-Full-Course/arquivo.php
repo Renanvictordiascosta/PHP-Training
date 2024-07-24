c9297dcd2db05aac155e0faf6be0e558411ecc2c
Renan Victor <80129254+Renanvictordiascosta@users.noreply.github.com>
2024-07-24 17:51:54 -0300

    Add files via upload

---
+++ PHP-Full-Course/arquivo.php
@@ -0,0 +1,7 @@
+<?php
+
+  // Validação do Nome e Senha
+
+  echo "Nome: " . htmlspecialchars($_GET["nome"]) . "<br>" . "Senha: " . htmlspecialchars($_GET["senha"]);
+
+?>
---
+++ PHP-Full-Course/index.php
@@ -0,0 +1,37 @@
+<!-- Criação do forms onde as informações serão inseridas-->
+
+<!DOCTYPE html>
+
+<html lang = "pt">
+
+  <head>
+
+    <meta charset = "UTF-8">
+
+    <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+
+    <title>Documento</title>
+
+  </head>
+
+  <body>
+
+    <form action = "arquivo.php" method = "GET">
+
+      <label>Nome de Usuário:</label>
+
+      <input type = "text" name = "nome"> <br>
+
+      <label>Senha:</label>
+
+      <input type = "password" name = "senha"> <br>
+
+      <input type = "submit" value = "Login">
+
+    </form>
+
+  </body>
+
+</html>
